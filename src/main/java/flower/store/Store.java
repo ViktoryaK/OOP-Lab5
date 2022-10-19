846241b6691e8e2c7f36001fb1962114573d8e2e
root <root@DESKTOP-M621CJ0.localdomain>
2022-10-19 17:39:25 +0300

    to

---
+++ src/main/java/flower/store/Store.java
@@ -0,0 +1,29 @@
+package flower.store;
+
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+public class Store {
+
+    public Hashtable<Flower, Integer> assortment;
+
+    public Store() {
+        this.assortment = new Hashtable<>();
+    }
+
+    public void add(Flower flower, int number) {
+        if (!assortment.containsKey(flower)) {
+            assortment.put(flower, number);
+        }
+        else {
+            assortment.put(flower, assortment.get(flower) + number);
+        }
+
+
+
+    }
+
+    public int search(Flower flower) {
+        return assortment.get(flower);
+    }
+}
